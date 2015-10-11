f4d3fd72fc75cb4c3bc57a35601e7527ba8270c5
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-10-11 16:36:33 -0400

    start of algorithm

---
+++ build.sh
@@ -0,0 +1,2 @@
+clang-format -i main.cpp
+clang++ -std=c++14 -O2 main.cpp -o sequence-alignment
---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int similarity_score(char a, char b) {
+  int score;
+  if (a == b) {
+    score = 1;
+  } else {
+    score = -1;
+  }
+}
+
+string slurp(const string &filename) {
+  ifstream in(filename, ifstream::in);
+  stringstream sstr;
+  sstr << in.rdbuf();
+  string file = sstr.str();
+  return file;
+}
+
+int main(int argc, char *argv[]) {
+  int c;
+  bool fexists = false;
+  bool showtime = false;
+  string file1 = argv[0];
+  string file2 = argv[1];
+  if (argc > 1) {
+    for (c = 1; c < argc; c++) {
+      if (strcmp(argv[c], "-t") == 0) {
+        showtime = true;
+      }
+    }
+  }
+
+  if (fexists == false) {
+    cout << "Usage: sequence-alignment filename_sequence_a filename_sequence_b "
+            "[-t]";
+  }
+
+  clock_t begin_read = clock();
+  string seq_a = slurp(file1);
+  string seq_b = slurp(file2);
+  clock_t end_read = clock();
+  double elapsed_secs_read = double(end_read - begin_read) / CLOCKS_PER_SEC;
+
+  // create the matrices
+  size_t length_m = seq_a.size();
+  size_t length_n = seq_b.size();
+  int H_matrix[length_m + 1][length_n + 1];
+
+  // set first row and first column to zero
+  for (int i = 0; i > length_m + 1; i++) {
+    H_matrix[0,i] = 0;
+  }
+  for (int j = 0, j > length_n + 1, j++) {
+    H_matrix[j,0] = 0;
+  }
+
+
+
+  return 0;
+}
