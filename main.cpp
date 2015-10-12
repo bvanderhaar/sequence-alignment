5b2c45603bfb2e62d1b5d674fff136804507ab30
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-10-11 23:13:39 -0400

    non threaded version

--- main.cpp
+++ main.cpp
@@ -3,15 +3,20 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <pthread.h>
 
 using namespace std;
+
+#define MATCH 1
+#define GAP_PENALTY 2
+#define MISMATCH_PENALTY 1
 
 int similarity_score(char a, char b) {
   int score;
   if ((a == b) || (a == '?' || b == '?')) {
-    score = 2;
+    score = MATCH;
   } else {
-    score = -1;
+    score = -MISMATCH_PENALTY;
   }
   return score;
 }
@@ -63,35 +68,40 @@ int main(int argc, char *argv[]) {
   // create the matrices
   size_t length_m = seq_a.size();
   size_t length_n = seq_b.size();
-  cout << "length m " << length_m << endl;
-  cout << "length n " << length_n << endl;
-  int H_matrix[length_m + 1][length_n + 1];
+  // cout << "length m " << length_m << endl;
+  // cout << "length n " << length_n << endl;
+  int H_matrix[length_m][length_n];
 
   // set first row and first column to zero
-  for (int i = 0; i < length_m + 1; i++) {
+  for (int i = 0; i < length_m; i++) {
     H_matrix[0][i] = 0;
   }
-  for (int j = 0; j < length_n + 1; j++) {
+  for (int j = 0; j < length_n; j++) {
     H_matrix[j][0] = 0;
   }
 
   int max_options[3];
-  for (int i = 1; i < length_m + 1; i++) {
-    for (int j = 1; j < length_n + 1; j++) {
+  clock_t begin_matrix_calc = clock();
+  for (int i = 1; i < length_m; i++) {
+    for (int j = 1; j < length_n; j++) {
       max_options[0] = H_matrix[i - 1][j - 1] +
                        similarity_score(seq_a.at(i - 1), seq_b.at(j - 1));
-      max_options[1] = H_matrix[i - 1][j] - 2;
-      max_options[2] = H_matrix[i][j - 1] - 2;
+      max_options[1] = H_matrix[i - 1][j] - GAP_PENALTY;
+      max_options[2] = H_matrix[i][j - 1] - GAP_PENALTY;
       H_matrix[i][j] = max_score(max_options);
     }
   }
+  clock_t end_matrix_calc = clock();
+  double elapsed_secs_matrix_calc =
+      double(end_matrix_calc - begin_matrix_calc) / CLOCKS_PER_SEC;
 
-  for (int i = 0; i < length_m + 1; i++) {
-    for (int j = 0; j < length_n + 1; j++) {
+  for (int i = 0; i < length_m; i++) {
+    for (int j = 0; j < length_n; j++) {
       cout << H_matrix[i][j] << ' ';
     }
     cout << endl;
   }
-
+  
+  cout << "Elapsed Calculation Time: " << elapsed_secs_matrix_calc << endl;
   return 0;
 }
