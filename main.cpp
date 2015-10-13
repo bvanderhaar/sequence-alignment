50d661be6c2b26f9fe90aea4940763f04b15963b
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-10-13 00:03:44 -0400

    threaded algorithm

--- main.cpp
+++ main.cpp
@@ -3,22 +3,18 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <pthread.h>
+#include <thread>
 #include <vector>
 using namespace std;
 
-const int MATCH = 1;
-const int GAP_PENALTY = 2;
+const int MATCH = 2;
+const int GAP_PENALTY = 1;
 const int MISMATCH_PENALTY = 1;
 
 string GKnownSequence;
 string GSearchSequence;
+size_t GKnownSequenceLength;
 vector<vector<int>> GMatrix;
-
-struct process_matrix {
-  int known_sequence_slot;
-  int search_sequence_slot;
-};
 
 int similarity_score(char a, char b) {
   int score;
@@ -48,42 +44,37 @@ string slurp(const string &filename) {
   return file;
 }
 
-void *process_known_row(void *threadarg) {
-  struct process_matrix *matrix;
-  matrix = (struct process_matrix *)threadarg;
+void process_known_row(int search_sequence_slot) {
   int max_options[3];
-  if (GMatrix[matrix->search_sequence_slot - 1][matrix->known_sequence_slot] !=
-      -1) {
-    max_options[0] =
-        GMatrix[matrix->search_sequence_slot - 1][matrix->known_sequence_slot -
-                                                  1] +
-        similarity_score(GKnownSequence.at(matrix->known_sequence_slot - 1),
-                         GSearchSequence.at(matrix->search_sequence_slot - 1));
-    max_options[1] = GMatrix[(matrix->search_sequence_slot) -
-                             1][(matrix->known_sequence_slot)] -
-                     GAP_PENALTY;
-    max_options[2] =
-        GMatrix[matrix->search_sequence_slot][matrix->known_sequence_slot - 1] -
-        GAP_PENALTY;
-    GMatrix[matrix->search_sequence_slot][matrix->known_sequence_slot] =
-        max_score(max_options);
+  for (int i = 1; i < GKnownSequenceLength;) {
+    if (GMatrix[search_sequence_slot - 1][i] != -1) {
+      max_options[0] =
+          GMatrix[search_sequence_slot - 1][i - 1] +
+          similarity_score(GKnownSequence.at(i - 1),
+                           GSearchSequence.at(search_sequence_slot - 1));
+      max_options[1] = GMatrix[search_sequence_slot - 1][i] - GAP_PENALTY;
+      max_options[2] = GMatrix[search_sequence_slot][i - 1] - GAP_PENALTY;
+      GMatrix[search_sequence_slot][i] = max_score(max_options);
+      i++;
+    }
   }
-  return 0;
+  return;
 }
 
 int main(int argc, char *argv[]) {
   int c;
+  int numthreads = 2;
   bool showtime = false;
   bool showmatrix = false;
   string filename_known_sequence = argv[1];
   string filename_search_sequence = argv[2];
   if (argc > 1) {
     for (c = 1; c < argc; c++) {
-      if (strcmp(argv[c], "-t") == 0) {
-        showtime = true;
-      }
       if (strcmp(argv[c], "-p") == 0) {
         showmatrix = true;
+      }
+      if (strcmp(argv[c], "-t") == 0) {
+        numthreads = atoi(argv[c + 1]);
       }
     }
   }
@@ -91,12 +82,14 @@ int main(int argc, char *argv[]) {
   if (argc < 3) {
     cout << "Usage: sequence-alignment filename_known_sequence "
             "filename_search_sequence "
-            "[-t]";
+            "[-tp]";
     return 0;
   }
   cout << "Starting alignment with known sequence: " << filename_known_sequence
        << endl;
   cout << "Starting alignment with search sequence " << filename_search_sequence
+       << endl;
+  cout << "Starting alignment algorithm with " << numthreads << " thread(s)"
        << endl;
 
   clock_t begin_read = clock();
@@ -106,13 +99,13 @@ int main(int argc, char *argv[]) {
   double elapsed_secs_read = double(end_read - begin_read) / CLOCKS_PER_SEC;
 
   // create the matrices
-  size_t known_sequence_size = GKnownSequence.size();
+  GKnownSequenceLength = GKnownSequence.size();
   size_t search_sequence_size = GSearchSequence.size();
   GMatrix.resize(search_sequence_size,
-                 std::vector<int>(known_sequence_size, -1));
+                 std::vector<int>(GKnownSequenceLength, -1));
 
   // set first row to zero
-  for (int i = 0; i < known_sequence_size; i++) {
+  for (int i = 0; i < GKnownSequenceLength; i++) {
     GMatrix[0][i] = 0;
   }
   // set first column to zero
@@ -121,12 +114,29 @@ int main(int argc, char *argv[]) {
   }
 
   clock_t begin_matrix_calc = clock();
-  struct process_matrix pm;
-  for (int i = 1; i < search_sequence_size; i++) {
-    for (int j = 1; j < known_sequence_size; j++) {
-      pm.known_sequence_slot = j;
-      pm.search_sequence_slot = i;
-      process_matrix_thread((void *)&pm);
+  if (numthreads == 1) {
+    for (int i = 1; i < search_sequence_size; i++) {
+      process_known_row(i);
+    }
+  } else {
+    // divide the work by numthreads
+    int cycles = search_sequence_size / numthreads;
+    vector<thread> t(numthreads);
+    int current_search_index = 1;
+    int started_threads;
+    for (int i = 0; i < cycles; i++) {
+      started_threads = 0;
+      for (int j = 0; j < numthreads; j++) {
+        if (current_search_index < search_sequence_size) {
+          t[j] = std::thread(process_known_row, current_search_index);
+          started_threads++;
+        }
+        current_search_index++;
+      }
+      // Join the threads with the main thread
+      for (int k = 0; k < started_threads; k++) {
+        t[k].join();
+      }
     }
   }
   clock_t end_matrix_calc = clock();
@@ -135,14 +145,24 @@ int main(int argc, char *argv[]) {
 
   if (showmatrix) {
     for (int i = 0; i < search_sequence_size; i++) {
-      for (int j = 0; j < known_sequence_size; j++) {
+      for (int j = 0; j < GKnownSequenceLength; j++) {
         cout << GMatrix[i][j] << ' ';
       }
       cout << endl;
     }
   }
 
+  int best_score = 0;
+  for (int i = 0; i < search_sequence_size; i++) {
+    for (int j = 0; j < GKnownSequenceLength; j++) {
+      if (GMatrix[i][j] > best_score) {
+        best_score = GMatrix[i][j];
+      }
+    }
+  }
+
   cout << "Elapsed Calculation Time: " << elapsed_msecs_matrix_calc << " ms"
        << endl;
+  cout << "Max Score: " << best_score << endl;
   return 0;
 }
